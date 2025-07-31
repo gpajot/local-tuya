db83730e1f5744552243f2fb56daaa7cf4cddf70
Gabriel Pajot <github@gab.lescactus.eu>
2025-07-31 15:45:42 +0200

    chore: use concurrent_tasks.RobustStream

--- local_tuya/tuya/transport.py
+++ local_tuya/tuya/transport.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
-from contextlib import AbstractAsyncContextManager
-from typing import Optional, cast
+from contextlib import AsyncExitStack
+from functools import partial
 
-from concurrent_tasks import BackgroundTask
+from concurrent_tasks import BackgroundTask, RobustStream
+from typing_extensions import Self  # 3.11
 
 from local_tuya.backoff import Backoff
 from local_tuya.events import EventNotifier
@@ -18,7 +19,7 @@ from local_tuya.tuya.events import (
 logger = logging.getLogger(__name__)
 
 
-class Transport(AbstractAsyncContextManager, asyncio.Protocol):
+class TuyaStream(RobustStream):
     def __init__(
         self,
         name: str,
@@ -28,100 +29,55 @@ class Transport(AbstractAsyncContextManager, asyncio.Protocol):
         timeout: float,
         event_notifier: EventNotifier,
     ):
+        super().__init__(
+            connector=partial(
+                asyncio.get_running_loop().create_connection,
+                address=address,
+                port=port,
+            ),
+            name=name,
+            backoff=backoff.wait,
+            timeout=timeout,
+        )
+        self._notifier = event_notifier
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._notifier.emit(TuyaConnectionClosed(None))
+        await super().__aexit__(exc_type, exc_val, exc_tb)
+
+    async def _connect(self) -> None:
+        if self._last_exc:
+            await self._notifier.emit(TuyaConnectionClosed(self._last_exc))
+        await super()._connect()
+        await self._notifier.emit(TuyaConnectionEstablished())
+
+
+class Transport(AsyncExitStack):
+    def __init__(
+        self,
+        name: str,
+        address: str,
+        port: int,
+        separator: bytes,
+        backoff: Backoff,
+        timeout: float,
+        event_notifier: EventNotifier,
+    ):
+        super().__init__()
+        self._stream = TuyaStream(name, address, port, backoff, timeout, event_notifier)
         event_notifier.register(TuyaDataSent, self._write)
         event_notifier.register(TuyaResponseReceived, self._connection_live)
         self._name = name
-        self._address = address
-        self._port = port
         self._backoff = backoff
-        self._timeout = timeout
+        self._separator = separator
         self._notifier = event_notifier
 
-        self._transport: Optional[asyncio.transports.WriteTransport] = None
-        self._closing = False
-        self._closed = asyncio.Event()
-        self._closed.set()
-        self._close_exc: Optional[Exception] = None
-        self._connected = asyncio.Event()
-        self._connect_task = BackgroundTask(self._connect)
         self._receive_task = BackgroundTask(self._receive)
-        # We need a queue as `data_received` is not async.
-        self._received_bytes: asyncio.Queue[bytes] = asyncio.Queue()
 
-    async def __aenter__(self):
-        self._closed.clear()
-        self._connect_task.create()
+    async def __aenter__(self) -> Self:
+        await self.enter_async_context(self._stream)
+        self.enter_context(self._receive_task)
         return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
-        self._closing = True
-        self._connect_task.cancel()
-        self._receive_task.cancel()
-        self._received_bytes = asyncio.Queue()
-        if self._transport:
-            await self._notifier.emit(TuyaConnectionClosed(None))
-            self._transport.close()
-            self._transport = None
-            self._connected.clear()
-            # Wait until `connection_lost` was called.
-            try:
-                await asyncio.wait_for(self._closed.wait(), timeout=self._timeout)
-            except asyncio.TimeoutError:
-                logger.error("%s: timeout waiting for transport to close", self._name)
-
-    async def _connect(self) -> None:
-        # Handle errors here as `connection_lost` is not async.
-        if self._close_exc:
-            exc = self._close_exc
-            self._close_exc = None
-            await self._notifier.emit(TuyaConnectionClosed(exc))
-        while True:
-            await self._backoff.wait()
-            try:
-                self._transport = await self._get_transport()
-                break
-            except Exception:
-                logger.warning(
-                    "%s: could not connect, retrying...", self._name, exc_info=True
-                )
-        self._connected.set()
-        logger.info(
-            "%s: connected to device %s:%i", self._name, self._address, self._port
-        )
-        await self._notifier.emit(TuyaConnectionEstablished())
-        self._receive_task.create()
-
-    async def _get_transport(self) -> asyncio.transports.WriteTransport:
-        transport, _ = await asyncio.wait_for(
-            asyncio.get_running_loop().create_connection(
-                lambda: self,
-                self._address,
-                self._port,
-            ),
-            self._timeout,
-        )
-        return cast(asyncio.transports.WriteTransport, transport)
-
-    def connection_lost(self, exc: Optional[Exception]) -> None:
-        self._close_exc = exc
-        if not exc:
-            logger.info(
-                "%s: disconnected from device %s:%i",
-                self._name,
-                self._address,
-                self._port,
-            )
-            # Notify the transport was properly closed.
-            self._closed.set()
-        else:
-            logger.warning("%s: connection lost: %s, reconnecting...", self._name, exc)
-            self._connect_task.cancel()
-            self._receive_task.cancel()
-            # Attempt to reconnect.
-            self._connected.clear()
-            self._transport = None
-            self._received_bytes = asyncio.Queue()
-            self._connect_task.create()
 
     def _connection_live(self, _: TuyaResponseReceived) -> None:
         # While this event has not been received, we assume the connection is not necessarily healthy.
@@ -130,19 +86,11 @@ class Transport(AbstractAsyncContextManager, asyncio.Protocol):
         # As long as it is unhealthy, connection attempts will increase the backoff.
         self._backoff.reset()
 
-    def data_received(self, data: bytes) -> None:
-        self._received_bytes.put_nowait(data)
-
     async def _receive(self) -> None:
+        reader = self._stream.reader
         while True:
-            data = await self._received_bytes.get()
+            data = await reader.readuntil(self._separator)
             await self._notifier.emit(TuyaDataReceived(data))
 
     async def _write(self, data: TuyaDataSent) -> None:
-        if self._closed.is_set():
-            raise RuntimeError("transport is closed")
-        if self._closing:
-            raise RuntimeError("transport is closing")
-        await self._connected.wait()
-        # Writing is asynchronous, errors will be raised through the `connection_lost` method.
-        cast(asyncio.transports.WriteTransport, self._transport).write(data)
+        await self._stream.write(data)
--- tests/tuya/test_transport.py
+++ tests/tuya/test_transport.py
@@ -4,14 +4,10 @@ import pytest
 
 from local_tuya.backoff import Backoff
 from local_tuya.tuya.events import (
-    TuyaConnectionClosed,
-    TuyaConnectionEstablished,
     TuyaDataReceived,
     TuyaDataSent,
-    TuyaResponseReceived,
 )
-from local_tuya.tuya.message.messages import EmptyResponse
-from local_tuya.tuya.transport import Transport
+from local_tuya.tuya.transport import Transport, TuyaStream
 
 
 class TestTransport:
@@ -20,81 +16,46 @@ class TestTransport:
         return mocker.MagicMock(spec=Backoff)
 
     @pytest.fixture
-    async def transport(self, backoff, notifier):
+    def reader(self, mocker):
+        return mocker.Mock(spec=asyncio.StreamReader)
+
+    @pytest.fixture
+    def stream(self, mocker, reader):
+        cls = mocker.patch("local_tuya.tuya.transport.TuyaStream")
+        s = mocker.MagicMock(spec=TuyaStream)
+        s.reader = reader
+        cls.return_value = s
+        return s
+
+    @pytest.fixture
+    async def transport(self, backoff, notifier, stream):
         return Transport(
             name="test",
             address="address",
             port=6666,
+            separator=b"z",
             backoff=backoff,
             timeout=5,
             event_notifier=notifier,
         )
 
-    @pytest.fixture
-    async def transport_future(self):
-        return asyncio.Future()
-
-    @pytest.fixture
-    def mock_transport(self, mocker, transport, transport_future):
-        mock = mocker.Mock(spec=asyncio.transports.WriteTransport)
-
-        async def _get_transport():
-            return await transport_future
-
-        mocker.patch.object(transport, "_get_transport", new=_get_transport)
-        mock.close.side_effect = lambda: transport._closed.set()
-        return mock
-
-    @pytest.fixture
-    async def connected_transport(
-        self,
-        transport,
-        mock_transport,
-        transport_future,
-        notifier_spy,  # To spy the connection established event.
-    ):
-        transport_future.set_result(mock_transport)
+    async def test_write(self, notifier, transport, stream):
         async with transport:
-            await transport._connected.wait()
-            yield transport
+            await notifier.emit(TuyaDataSent(b"\x00"))
+        stream.write.assert_called_once_with(b"\x00")
 
-    @pytest.mark.usefixtures("transport")
-    async def test_write_disconnected(self, notifier):
-        with pytest.raises(RuntimeError, match="transport is closed"):
-            await notifier.emit(TuyaDataSent(b""))
+    async def test_receive(self, notifier, transport, reader, assert_event_emitted):
+        returned = False
 
-    async def test_write_connected(self, connected_transport, notifier, mock_transport):
-        await notifier.emit(TuyaDataSent(b"\x00"))
-        mock_transport.write.assert_called_once_with(b"\x00")
+        async def _read(_):
+            nonlocal returned
+            if not returned:
+                returned = True
+                return b"\x00z"
+            await asyncio.sleep(1)
+            return b"\x00z"
 
-    async def test_write_connecting(
-        self, notifier, transport, mock_transport, transport_future
-    ):
+        reader.readuntil.side_effect = _read
         async with transport:
-            send_task = asyncio.create_task(notifier.emit(TuyaDataSent(b"\x00")))
-            await asyncio.sleep(0.001)  # context switch.
-            mock_transport.write.assert_not_called()
-            transport_future.set_result(mock_transport)
-            await asyncio.sleep(0.001)  # context switch.
-            mock_transport.write.assert_called_once_with(b"\x00")
-            # Cleanup.
-            await send_task
-
-    async def test_receive(self, notifier, connected_transport, assert_event_emitted):
-        connected_transport.data_received(b"\x00")
-        await asyncio.sleep(0)  # context switch.
-        assert_event_emitted(TuyaDataReceived(b"\x00"), 1)
-
-    async def test_reconnect(
-        self, connected_transport, assert_event_emitted, backoff, notifier
-    ):
-        assert_event_emitted(TuyaConnectionEstablished(), 1)
-        error = ConnectionResetError()
-        connected_transport.connection_lost(error)
-        await asyncio.sleep(0)  # context switch.
-        assert_event_emitted(TuyaConnectionClosed(error), 1)
-        assert_event_emitted(TuyaConnectionEstablished(), 2)
-        assert backoff.wait.call_count == 2
-        backoff.reset.assert_not_called()
-        await notifier.emit(TuyaResponseReceived(0, EmptyResponse(), None))
-        backoff.reset.assert_called_once()
+            await asyncio.sleep(0)  # context switch.
+        assert_event_emitted(TuyaDataReceived(b"\x00z"), 1)
