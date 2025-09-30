8fa49b1ab38386a66e15b45c7f51ee98906e9897
Gabriel Pajot <github@gab.lescactus.eu>
2025-09-30 09:30:28 +0200

    chore: simplify receiver

--- tests/tuya/test_receiver.py
+++ tests/tuya/test_receiver.py
@@ -1,7 +1,6 @@
 import pytest
 
 from local_tuya.tuya.events import (
-    TuyaConnectionClosed,
     TuyaDataReceived,
     TuyaResponseReceived,
 )
@@ -9,44 +8,33 @@ from local_tuya.tuya.message import MessageHandler, UpdateCommand, UpdateRespons
 from local_tuya.tuya.receiver import Receiver
 
 
-class TestReceiver:
-    @pytest.fixture
-    def msg_handler(self, mocker):
-        return mocker.Mock(spec=MessageHandler)
+@pytest.fixture
+def msg_handler(mocker):
+    return mocker.Mock(spec=MessageHandler)
 
-    @pytest.fixture
-    def receiver(self, msg_handler, notifier):
-        return Receiver("test", msg_handler, notifier)
 
-    async def test_buffer(self, receiver, msg_handler, notifier):
-        msg_handler.unpack.side_effect = lambda d: (0, None, None, d)
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        await notifier.emit(TuyaConnectionClosed(None))
-        msg_handler.unpack.side_effect = lambda d: (0, None, None, b"")
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        msg_handler.unpack.side_effect = ValueError
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        await notifier.emit(TuyaDataReceived(b"\x00"))
+@pytest.fixture(autouse=True)
+def receiver(msg_handler, notifier):
+    return Receiver("test", msg_handler, notifier)
 
-        assert msg_handler.unpack.call_count == 6
-        msg_handler.unpack.assert_has_calls(
-            (
-                ((b"\x00",), {}),
-                ((b"\x00\x00",), {}),
-                ((b"\x00",), {}),
-                ((b"\x00",), {}),
-                ((b"\x00",), {}),
-                ((b"\x00",), {}),
-            )
-        )
 
-    async def test_response_received(
-        self, receiver, msg_handler, notifier, assert_event_emitted
-    ):
-        msg_handler.unpack.return_value = (1, UpdateResponse(), UpdateCommand, b"")
-        await notifier.emit(TuyaDataReceived(b"\x00"))
-        assert_event_emitted(
-            TuyaResponseReceived(1, UpdateResponse(), UpdateCommand), 1
-        )
+async def test_no_response(msg_handler, notifier, assert_event_emitted):
+    msg_handler.unpack.side_effect = lambda d: (0, None, None, d)
+    await notifier.emit(TuyaDataReceived(b"\x00"))
+    msg_handler.unpack.side_effect = lambda d: (0, None, None, b"")
+    await notifier.emit(TuyaDataReceived(b"\x01"))
+
+    def _raise(data: bytes):
+        raise ValueError(data.decode("utf-8"))
+
+    msg_handler.unpack.side_effect = _raise
+    with pytest.raises(ValueError, match=b"\x04".decode("utf-8")):
+        await notifier.emit(TuyaDataReceived(b"\x04"))
+
+    assert_event_emitted(TuyaResponseReceived(1, UpdateResponse(), UpdateCommand), 0)
+
+
+async def test_response(msg_handler, notifier, assert_event_emitted):
+    msg_handler.unpack.return_value = (1, UpdateResponse(), UpdateCommand, b"")
+    await notifier.emit(TuyaDataReceived(b"\x00"))
+    assert_event_emitted(TuyaResponseReceived(1, UpdateResponse(), UpdateCommand), 1)
